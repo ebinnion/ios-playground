7bb3761f1ee27088a4e5a2fccc54e588ad4598b0
Eric Binnion <ericbinnion@gmail.com>
2015-04-05 01:44:44 -0500

    Adds CustomTableViewCellExample

---
+++ CustomTableViewCellExample/CustomTableViewCellExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTableViewCellExample
+//
+//  Created by Eric Binnion on 4/5/15.
+//  Copyright (c) 2015 Eric Binnion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
