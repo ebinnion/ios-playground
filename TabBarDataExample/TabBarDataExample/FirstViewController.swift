504b35f774b74add434267002527f5d07ee4f830
Eric Binnion <ericbinnion@gmail.com>
2015-04-05 01:23:49 -0500

    Adds TabBarDataExample

---
+++ TabBarDataExample/TabBarDataExample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabBarDataExample
+//
+//  Created by Eric Binnion on 4/5/15.
+//  Copyright (c) 2015 Eric Binnion. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TabBarDataExample/TabBarDataExample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabBarDataExample
+//
+//  Created by Eric Binnion on 4/5/15.
+//  Copyright (c) 2015 Eric Binnion. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
